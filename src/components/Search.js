4f6a860c145b9c662ce5e77c5e6dcf9bafb87c3d
zivile.utkeviciute <zivile.utkeviciute@agillic.com>
2020-08-21 02:37:54 +0200

    Bug fixes

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,6 +11,7 @@ const Search = ({label, placeholder}) => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [results, setResults] = useState([])
   const [pageNumber, setPageNumber] = useState(1)
+  const [hasMore, setHasMore] = useState(false)
   const inputReference = useRef()
 
   let cancelRequest
@@ -26,14 +27,19 @@ const Search = ({label, placeholder}) => {
       if (result) {
         const data = result.data.photos
         const images = mapImageUrls(data.photo)
-        if (data.pages === 0) {
+        (data.pages > data.page)
+          ? setHasMore(true)
+          : setHasMore(false)
+
+        if ((data.pages === 0)) {
           setErrorMessage('No search results found')
         }
-        if (data.pages === data.page) {
+        if (!hasMore && (data.pages === data.page)) {
           setErrorMessage('There are no more search results')
         }
         setResults([...results, ...images])
         setLoading(false)
+        console.log(data)
       }
     }).catch(function (thrown) {
       if (axios.isCancel(thrown)) {
@@ -48,23 +54,33 @@ const Search = ({label, placeholder}) => {
   // Checks if the page has scrolled to the bottom and increases the page number to trigger data fetching
   window.onscroll = debounce(() => {
     const {scrollTop, offsetHeight} = document.documentElement
-    if (errorMessage || loading ) return;
+
+    if (errorMessage || loading || !hasMore) return
     if (window.innerHeight + scrollTop === offsetHeight) {
+      hasMore && setPageNumber(pageNumber + 1)
       setLoading(true)
-      setPageNumber(pageNumber + 1)
     }
   }, 500)
 
-  // A bit ugly, but very reliable
+  // A bit ugly, but reliable
   const handleChange = value => {
-    cancelRequest && cancelRequest.cancel();
+    cancelRequest && cancelRequest.cancel()
+
+    if (value && results.length > 0) {
+      setPageNumber(1)
+      setResults([])
+      setErrorMessage(null)
+      setSearchValue(value)
+      setLoading(true)
+    }
     if (!value) {
       setSearchValue('')
       setResults([])
       setPageNumber(1)
       setErrorMessage(null)
       setLoading(false)
-    } else {
+    }
+    if (value) {
       setSearchValue(value)
       setLoading(true)
     }
@@ -87,7 +103,7 @@ const Search = ({label, placeholder}) => {
     }
   }, [])
 
-  //Fetches the results if searchValue or pageNumber changes. There's no need to have fetchSearchResults in the dependency array as it's only called here.
+  //Fetches the results only if searchValue or pageNumber changes
   useEffect(() => {
     searchValue && fetchSearchResults(searchValue, pageNumber)
   // eslint-disable-next-line react-hooks/exhaustive-deps
